1efed497ae60925a2087445d6f1a8a477f0d48f1
adamzwickey <azwickey@vmware.com>
2020-10-03 13:37:07 -0400

    added script to clean up env

---
+++ scripts/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+INSTANCE_ID=$(yq r ec2.yaml -d0 'Instances[0].InstanceId')
+echo Deleting instance $INSTANCE_ID
+aws ec2 terminate-instances --instance-ids $INSTANCE_ID --output yaml
+
+LT_ID=$(yq r lt.yaml LaunchTemplate.LaunchTemplateId)
+echo Deleting Launch Template $LT_ID
+aws ec2 delete-launch-template --launch-template-id $LT_ID --region us-east-2 --output yaml
