d054162676fece97efbf71f8cf6f638e28d56c06
adamzwickey <azwickey@vmware.com>
2020-10-03 17:12:53 -0400

    fixed error in tkg command

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -25,25 +25,25 @@ tar -zxvf tkg-extensions-manifests-*
 echo $HOME
 tkg get mc
 # Write needed values into TKG config file
-yq write $HOME/config.yaml -i "SERVICE_CIDR" 100.64.0.0/13
-yq write $HOME/config.yaml -i "CLUSTER_CIDR" 100.96.0.0/11
-yq write $HOME/config.yaml -i "MACHINE_HEALTH_CHECK_ENABLED" true
-yq write $HOME/config.yaml -i "AWS_B64ENCODED_CREDENTIALS" ""
-yq write $HOME/config.yaml -i "AWS_PUBLIC_SUBNET_ID" $(yq r $VARS_YAML aws.publicSubnet1)
-yq write $HOME/config.yaml -i "AWS_PUBLIC_SUBNET_ID_1" $(yq r $VARS_YAML aws.publicSubnet2)
-yq write $HOME/config.yaml -i "AWS_PUBLIC_SUBNET_ID_2" $(yq r $VARS_YAML aws.publicSubnet3)
-yq write $HOME/config.yaml -i "AWS_PRIVATE_SUBNET_ID" $(yq r $VARS_YAML aws.privateSubnet1)
-yq write $HOME/config.yaml -i "AWS_PRIVATE_SUBNET_ID_1" $(yq r $VARS_YAML aws.privateSubnet2)
-yq write $HOME/config.yaml -i "AWS_PRIVATE_SUBNET_ID_2" $(yq r $VARS_YAML aws.privateSubnet3)
-yq write $HOME/config.yaml -i "AWS_NODE_AZ" us-east-2a
-yq write $HOME/config.yaml -i "AWS_NODE_AZ_1" us-east-2b
-yq write $HOME/config.yaml -i "AWS_NODE_AZ_2" us-east-2c
-yq write $HOME/config.yaml -i "CONTROL_PLANE_MACHINE_TYPE" t3.medium
-yq write $HOME/config.yaml -i "NODE_MACHINE_TYPE" t3.xlarge
-yq write $HOME/config.yaml -i "AWS_SSH_KEY_NAME" $(yq r $VARS_YAML aws.keyPairName)
-yq write $HOME/config.yaml -i "AWS_VPC_ID" $(yq r $VARS_YAML aws.vpcId)
-yq write $HOME/config.yaml -i "AWS_REGION" us-east-2
-cat $HOME/config.yaml
+yq write $HOME/.tkg/config.yaml -i "SERVICE_CIDR" 100.64.0.0/13
+yq write $HOME/.tkg/config.yaml -i "CLUSTER_CIDR" 100.96.0.0/11
+yq write $HOME/.tkg/config.yaml -i "MACHINE_HEALTH_CHECK_ENABLED" true
+yq write $HOME/.tkg/config.yaml -i "AWS_B64ENCODED_CREDENTIALS" ""
+yq write $HOME/.tkg/config.yaml -i "AWS_PUBLIC_SUBNET_ID" $(yq r $VARS_YAML aws.publicSubnet1)
+yq write $HOME/.tkg/config.yaml -i "AWS_PUBLIC_SUBNET_ID_1" $(yq r $VARS_YAML aws.publicSubnet2)
+yq write $HOME/.tkg/config.yaml -i "AWS_PUBLIC_SUBNET_ID_2" $(yq r $VARS_YAML aws.publicSubnet3)
+yq write $HOME/.tkg/config.yaml -i "AWS_PRIVATE_SUBNET_ID" $(yq r $VARS_YAML aws.privateSubnet1)
+yq write $HOME/.tkg/config.yaml -i "AWS_PRIVATE_SUBNET_ID_1" $(yq r $VARS_YAML aws.privateSubnet2)
+yq write $HOME/.tkg/config.yaml -i "AWS_PRIVATE_SUBNET_ID_2" $(yq r $VARS_YAML aws.privateSubnet3)
+yq write $HOME/.tkg/config.yaml -i "AWS_NODE_AZ" us-east-2a
+yq write $HOME/.tkg/config.yaml -i "AWS_NODE_AZ_1" us-east-2b
+yq write $HOME/.tkg/config.yaml -i "AWS_NODE_AZ_2" us-east-2c
+yq write $HOME/.tkg/config.yaml -i "CONTROL_PLANE_MACHINE_TYPE" t3.medium
+yq write $HOME/.tkg/config.yaml -i "NODE_MACHINE_TYPE" t3.xlarge
+yq write $HOME/.tkg/config.yaml -i "AWS_SSH_KEY_NAME" $(yq r $VARS_YAML aws.keyPairName)
+yq write $HOME/.tkg/config.yaml -i "AWS_VPC_ID" $(yq r $VARS_YAML aws.vpcId)
+yq write $HOME/.tkg/config.yaml -i "AWS_REGION" us-east-2
+cat $HOME/.tkg/config.yaml
 
 MGMT_PLAN=$(yq r $VARS_YAML tkg.mgmt.plan)
 # tkg init -i aws -p $MGMT_PLAN --ceip-participation false --name aws-mgmt --cni antrea -v 8
