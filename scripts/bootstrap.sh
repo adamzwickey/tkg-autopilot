a88d5f321707cbc55950e54eb9ec7ac4416605d5
adamzwickey <azwickey@vmware.com>
2020-10-05 10:20:57 -0400

    adding gitops reqs

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -21,7 +21,7 @@ mv tkg/ytt-linux-* /usr/bin/ytt
 # TKG Extensions
 tar -zxvf tkg-extensions-manifests-*
 rm *.tar.gz
-mv tkg-extensions-manifests-* tkg-extensions-manifests
+mv tkg-extensions-* tkg-extensions-manifests
 
 # Initialize TKG
 tkg get mc
