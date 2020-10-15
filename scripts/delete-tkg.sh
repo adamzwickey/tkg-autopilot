1f3980af689a83397f12b1742ec0339af77fccd9
adamzwickey <azwickey@vmware.com>
2020-10-15 10:20:27 -0400

    enhanced cleaup of clusters

--- scripts/delete-tkg.sh
+++ scripts/delete-tkg.sh
@@ -6,4 +6,11 @@ export AWS_ACCESS_KEY_ID=$(yq r $VARS_YAML aws.accessKey)
 export AWS_SECRET_ACCESS_KEY=$(yq r $VARS_YAML aws.secretKey)
 export AWS_REGION=$(yq r $VARS_YAML aws.region)
 
+tkg delete cluster $(yq r $VARS_YAML tkg.workload1.name) -y 
+tkg delete cluster $(yq r $VARS_YAML tkg.workload2.name) -y 
+while tkg get clusters --config config.yaml | grep deleting  ; [ $? -eq 0 ]; do
+	echo Clusters still deleting
+	sleep 5s
+done
+
 tkg delete mc $(yq r $VARS_YAML tkg.mgmt.name) -y
